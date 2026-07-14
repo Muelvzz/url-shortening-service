d1b0c4710121a4907884ab55f50ca4b4477baa7c
Muelvin Lopez <muelvinlopez25@gmail.com>
2026-07-14 22:07:29 +0800

    refactor: improved code structure

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, HTTPException
 
 from ..db.database import insert_url
 from ..schemas.url_schema import UrlCreate, UrlOut
@@ -7,6 +7,14 @@ router = APIRouter()
 
 @router.post("/shorten", status_code=status.HTTP_201_CREATED, response_model=UrlOut)
 async def create_shorten_url(payload: UrlCreate):
-  insert_url_from_db = insert_url(payload.url)
+  try:
+    insert_url_from_db = insert_url(payload.url)
 
-  return insert_url_from_db
+    return insert_url_from_db
+  
+  except Exception as e:
+    print(f"Error occured: {e}")
+    raise HTTPException(
+      status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+      detail="an error occured while shortening the URL..."
+    )
--- app/utils/datetime_json.py
+++ app/utils/datetime_json.py
@@ -3,3 +3,9 @@ import json
 def convert_datetime_to_json(datetime):
   json_datetime = json.dumps(datetime.now(), default=str)
   return json_datetime
+
+if __name__ == "__main__":
+  from datetime import datetime
+
+  json_datetime = convert_datetime_to_json(datetime.now())
+  print(json_datetime)
